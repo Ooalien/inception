3e19669cf2b87c8887d9e0c6cb9f94cdf42e8ace
Ooalien <alibayar750@gmail.com>
2023-02-08 10:02:35 +0100

    all

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -2,61 +2,65 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
  *
  * This file contains the following configurations:
  *
- * * MySQL settings
+ * * Database settings
  * * Secret keys
  * * Database table prefix
  * * ABSPATH
  *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
+// ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name' );
+define( 'DB_NAME',  getenv('MYSQL_DB_NAME'));
 
-/** MySQL database username */
-define( 'DB_USER', 'username' );
+/** Database username */
+define( 'DB_USER', getenv('MYSQL_USER') );
 
-/** MySQL database password */
-define( 'DB_PASSWORD', 'password' );
+/** Database password */
+define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
 
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+/** Database hostname */
+define( 'DB_HOST', getenv('MYSQL_HOST') );
 
-/** Database Charset to use in creating database tables. */
+/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
-/** The Database Collate type. Don't change this if in doubt. */
+/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
 /**#@+
- * Authentication Unique Keys and Salts.
+ * Authentication unique keys and salts.
  *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         'JSV+q`rni23|F`^+7xPv)~`CL7SWy/sc&?zx6`^v3pS6TE9M~m}KgKiW<!8I;95P' );
+define( 'SECURE_AUTH_KEY',  'APz!Ta.Y^gL~c]UmF$mZFukzC#18PBWVXu1D>(GX,Ni+mA3;;CDTJAJv0@77dd#3' );
+define( 'LOGGED_IN_KEY',    'Rmn#*c8pmI^Uw5xcH,2b*WLn=7m93%mR[+pZ)4u3=DnA/l&}gxKS;YDL,~RR[4p<' );
+define( 'NONCE_KEY',        '1`/Z),#ex,!w8RLX/z#xMx$=iN=X[?&e31sI~&-P6DW]HM.%=3b@qQG1kuIyH^~G' );
+define( 'AUTH_SALT',        '{W[g&m(f[X3ZVr%!=RAR+1;.|U>9L@+Vc+dJp!M{=P:<< ,1)J=8}=e6<P?<!oV*' );
+define( 'SECURE_AUTH_SALT', 'N,9qA$N1yr:J1C=UO~NtHI=OPY/Cb~|%nU+JpU7:C+D>x{Ybuvz<Zlp+w@<m,8~O' );
+define( 'LOGGED_IN_SALT',   'qrI+=+W)PQlqf1lV$yJ@V^8>r%jVOIiEY)C={}h;:$~)3/T[cFMsv|ykrH&/CDgf' );
+define( 'NONCE_SALT',       '0TX=%?d6YceS(|@221$W#SrVTs/ulFJqm0M[OpS:Q},I8[)aL=<I4iGYITS61b,C' );
 
 /**#@-*/
 
 /**
- * WordPress Database Table prefix.
+ * WordPress database table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
@@ -77,6 +81,16 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
