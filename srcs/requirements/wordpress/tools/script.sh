341ff6c3d69d62ee436cd92041f08b712fd4fa49
Ooalien <alibayar750@gmail.com>
2023-02-21 09:41:56 +0100

    l3jb

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,7 +5,7 @@ chown www-data:www-data wp-config.php
 wp config set --allow-root 'WP_REDIS_HOST' "${REDIS_HOST}";
 wp config set --allow-root 'WP_REDIS_PORT' 6379;
 wp core install --url=https://localhost --title="AbayarIncEpTi0n" --admin_name=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=alibayar750@gmail.com --allow-root
-
+wp user create ${WP_USER} ${WP_USER_mail} --user_pass=${WP_USER_PASS} --role=${WP_USER_ROLE} --allow-root
 
 mkdir -p /run/php/
 touch /run/php/php7.3-fpm.sock
