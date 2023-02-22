61457f89fd5d9470b228abe8ea177d695be927ce
Ooalien <alibayar750@gmail.com>
2023-02-22 01:16:35 +0100

    hadchi naade

--- srcs/requirements/bonus/adminer/tools/entrypoint.sh
+++ srcs/requirements/bonus/adminer/tools/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-a2enconf adminer;
+# a2enconf adminer;
 mkdir /run/php/
 wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
 chown -R www-data:www-data /var/www/html/
