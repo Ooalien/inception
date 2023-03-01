51d1964cd1f9ec0c2df20d0304cad7a8c89a02c9
Ooalien <alibayar750@gmail.com>
2023-03-01 16:08:28 +0100

    backup rwina felcouzina

--- srcs/requirements/bonus/ftp-server/tools/script.sh
+++ srcs/requirements/bonus/ftp-server/tools/script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos '' ali
+useradd ali
 echo "ali" | tee -a /etc/vsftpd.userlist
 echo "ali:1234" | chpasswd
 usermod -aG www-data ali
 
 mkdir -p /home/ali/empty
-# mkdir -p /home/ali/wordpress
-chown ali:ali /home/ali/wordpress
-chown ali:ali /home/ali/empty
 
 exec vsftpd
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
 # wp core download --allow-root
-wp core config --dbhost=${MYSQL_HOST} --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
-chmod 600 wp-config.php
-chown www-data:www-data wp-config.php
-wp config set --allow-root 'WP_REDIS_HOST' "${REDIS_HOST}";
-wp config set --allow-root 'WP_REDIS_PORT' 6379;
-wp core install --url=https://localhost --title="AbayarIncEpTi0n" --admin_name=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=alibayar750@gmail.com --allow-root
-wp user create ${WP_USER} ${WP_USER_mail} --user_pass=${WP_USER_PASS} --role=${WP_USER_ROLE} --allow-root
+# mkdir helloworld
+# # wp  config create --dbhost=${MYSQL_HOST} --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
+# chmod 600 wp-config.php
+# chown www-data:www-data wp-config.php
+# wp config set --allow-root 'WP_REDIS_HOST' "${REDIS_HOST}";
+# wp config set --allow-root 'WP_REDIS_PORT' 6379;
+# wp core install --url=https://10.12.176.126 --title="AbayarIncEpTi0n" --admin_name=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=alibayar750@gmail.com --allow-root
+# wp user create ${WP_USER} ${WP_USER_mail} --user_pass=${WP_USER_PASS} --role=${WP_USER_ROLE} --allow-root
 
-mkdir -p /run/php/
-touch /run/php/php7.3-fpm.sock
-chown -R www-data:www-data /run/php/php7.3-fpm.sock
-chmod 660 /run/php/php7.3-fpm.sock
+# mkdir -p /run/php/
+# touch /run/php/php7.3-fpm.sock
+# chown -R www-data:www-data /run/php/php7.3-fpm.sock
+# chmod 660 /run/php/php7.3-fpm.sock
 
-wp plugin install --allow-root redis-cache --activate
-wp redis enable --allow-root
+# wp plugin install --allow-root redis-cache --activate
+# wp redis enable --allow-root
 
 
 exec $@
